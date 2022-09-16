1dd8cd30b2c1c1787b4626c6d6724450bf2b8795
viperxfury <80588039+viperxfury@users.noreply.github.com>
2022-09-16 17:09:44 +0545

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -441,7 +441,7 @@ def gdtot(url: str) -> str:
         raise DirectDownloadLinkException("ERROR: Try in your broswer, mostly file not found or user limit exceeded!")
     return f'https://drive.google.com/open?id={decoded_id}'
   
-  def mdis_k(urlx: str) -> str:
+  def mdisk(urlx):
     scraper = create_scraper(interpreter="nodejs", allow_brotli=False)
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"
@@ -455,9 +455,6 @@ def gdtot(url: str) -> str:
     return query
 
 def mdisk(url: str) -> str:
-    """MDisk DDL link generator
-    By https://github.com/missemily2022"""
-
     try:
         fxl = url.split("/")
         urlx = fxl[-1]
